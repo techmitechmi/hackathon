a4d8694a7a3c4ea4e0efb04296e19f6ea1c36a8c
rkwap <rkwap2011@gmail.com>
2019-01-28 22:56:11 +0530

    Added Captcha

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,28 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from functools import wraps
+from flask_recaptcha import ReCaptcha
+import requests
+import json
 
 app = Flask(__name__, instance_path=os.path.join(os.path.abspath(os.curdir), 'instance'), instance_relative_config=True, static_url_path="", static_folder="static")
 app.config.from_pyfile('config.cfg')
 app.config['SESSION_FILE_DIR'] = mkdtemp()
 mysql=MySQL(app)
 Session(app)
+
+recaptcha = ReCaptcha(app=app)
+
+def is_human(captcha_response):
+    """ Validating recaptcha response from google server
+        Returns True captcha test passed for submitted form else returns False.
+    """
+    secret = "6Le8-XUUAAAAAD3eT4zYW_i2pWEsJw_szKQwlyZc"
+    payload = {'response':captcha_response, 'secret':secret}
+    response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
+    response_text = json.loads(response.text)
+    return response_text['success']
+
 
 def execute_db(query,args=()):
     cur=mysql.connection.cursor()
--- app/views/main.py
+++ app/views/main.py
@@ -18,60 +18,65 @@ def register():
     if request.method == 'GET':
         return render_template("register.html")
     else:
-        team_name = request.form['team_name']
-        team_id = query_db("select team_id from teams where LOWER(team_name) = %s",(team_name.lower(),))
-        if team_id is not None:
-            flash("Team Name Already Exists!", 'danger')
-            print('Team Name Already Exists!')
-            return redirect(url_for("main.register"))
-        else:
-            rolls = []
-            names = []
-            years = []
-            for i in range(1,5):
-                if i !=4:
-                    rolls.append(request.form['roll'+str(i)])
-                    names.append(request.form['name'+str(i)])
-                    years.append(request.form['year'+str(i)])
-                else:
-                    if request.form['roll'+str(i)] is None or request.form['name'+str(i)] is None:
-                        rolls.append(None)
-                        names.append(None)
-                        years.append(None)
-                    else:
+        captcha_response = request.form['g-recaptcha-response']
+        if is_human(captcha_response):
+            team_name = request.form['team_name']
+            team_id = query_db("select team_id from teams where LOWER(team_name) = %s",(team_name.lower(),))
+            if team_id is not None:
+                flash("Team Name Already Exists!", 'danger')
+                print('Team Name Already Exists!')
+                return redirect(url_for("main.register"))
+            else:
+                rolls = []
+                names = []
+                years = []
+                for i in range(1,5):
+                    if i !=4:
                         rolls.append(request.form['roll'+str(i)])
                         names.append(request.form['name'+str(i)])
                         years.append(request.form['year'+str(i)])
+                    else:
+                        if request.form['roll'+str(i)] is None or request.form['name'+str(i)] is None:
+                            rolls.append(None)
+                            names.append(None)
+                            years.append(None)
+                        else:
+                            rolls.append(request.form['roll'+str(i)])
+                            names.append(request.form['name'+str(i)])
+                            years.append(request.form['year'+str(i)])
 
 
-            if len(rolls) > len(set(rolls)):
-                flash("Duplicate Entries not allowed!", "danger")
-                return redirect(url_for("main.register"))
-
-            student_check = []
-            for i in range(0,4):
-                student_check.append(query_db("select s"+str(i+1)+"_roll from teams where s1_roll=%s or s2_roll=%s or s3_roll=%s or s4_roll=%s",(rolls[i],rolls[i],rolls[i],rolls[i])))
-
-            for check in student_check:
-                if check is not None:
-                    flash("Some students already registered!", "danger")
+                if len(rolls) > len(set(rolls)):
+                    flash("Duplicate Entries not allowed!", "danger")
                     return redirect(url_for("main.register"))
 
-            execute_db("INSERT INTO teams(team_name, s1_roll, s2_roll, s3_roll, s4_roll, s1_name, s2_name, s3_name, s4_name, s1_year, s2_year, s3_year, s4_year) Values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(
-                team_name,
-                rolls[0],
-                rolls[1],
-                rolls[1],
-                rolls[3],
-                names[0],
-                names[1],
-                names[2],
-                names[3],
-                years[0],
-                years[1],
-                years[2],
-                years[3],
-            ))
-            flash("Applied Successfully", 'success')
+                student_check = []
+                for i in range(0,4):
+                    student_check.append(query_db("select s"+str(i+1)+"_roll from teams where s1_roll=%s or s2_roll=%s or s3_roll=%s or s4_roll=%s",(rolls[i],rolls[i],rolls[i],rolls[i])))
+
+                for check in student_check:
+                    if check is not None:
+                        flash("Some students already registered!", "danger")
+                        return redirect(url_for("main.register"))
+
+                execute_db("INSERT INTO teams(team_name, s1_roll, s2_roll, s3_roll, s4_roll, s1_name, s2_name, s3_name, s4_name, s1_year, s2_year, s3_year, s4_year) Values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(
+                    team_name,
+                    rolls[0],
+                    rolls[1],
+                    rolls[1],
+                    rolls[3],
+                    names[0],
+                    names[1],
+                    names[2],
+                    names[3],
+                    years[0],
+                    years[1],
+                    years[2],
+                    years[3],
+                ))
+                flash("Applied Successfully", 'success')
+                return redirect(url_for("main.register"))
+            return render_template("register.html")
+        else:
+            flash("Sorry! Bots not allowed.","danger")
             return redirect(url_for("main.register"))
-        return render_template("register.html")
